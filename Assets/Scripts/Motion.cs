ef410db5d04b3378f802b4c3fa55da0b6d2010dc
Visfire <Test@gmail.com>
2020-07-20 16:47:09 -0500

    added pistol, sprint, jump, etc.

--- Assets/Scripts/Motion.cs
+++ Assets/Scripts/Motion.cs
@@ -6,7 +6,14 @@ public class Motion : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
+    public float sprintModifier;
+    public Camera normalCam;
+    public float jumpForce;
     private Rigidbody rig;
+    private float baseFOV = 60f;
+    private float sprintFOVMod = 1.25f;
+    public LayerMask ground;
+    public Transform groundDetector;
     void Start()
     {
         Camera.main.enabled = false;
@@ -16,10 +23,37 @@ public class Motion : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float t_hmove = Input.GetAxis("Horizontal");
-        float t_vmove = Input.GetAxis("Vertical");
+        float t_hmove = Input.GetAxisRaw("Horizontal");
+        float t_vmove = Input.GetAxisRaw("Vertical");
+        bool sprint = Input.GetKey(KeyCode.LeftShift);
+        bool jump = Input.GetKey(KeyCode.Space);
+        bool isGrounded = Physics.Raycast(groundDetector.position,Vector3.down,0.1f,ground);
+        bool isJumping = jump;
+        if(isGrounded){
+            isJumping = jump;
+        }else{
+            isJumping = false;
+        }
+        bool isSprinting;
+        if(t_vmove > 0 && isJumping == false&& isGrounded == true){
+            isSprinting = sprint;
+        }else{
+            isSprinting = false;
+        }
+        if(isJumping){
+            rig.AddForce(Vector3.up * jumpForce);
+        }
         Vector3 t_direction = new Vector3(t_hmove,0,t_vmove);
         t_direction.Normalize();
-        rig.velocity = transform.TransformDirection(t_direction)*speed*Time.fixeddeltaTime;
+        float t_adjustedSpeed = speed;
+        if(isSprinting){
+            t_adjustedSpeed = t_adjustedSpeed * sprintModifier;
+            normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView,baseFOV*sprintFOVMod,Time.deltaTime*8);
+        }else{
+            normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView,baseFOV,Time.deltaTime*8);
+        }
+        Vector3 t_targetVelocity = transform.TransformDirection(t_direction)*t_adjustedSpeed*Time.fixedDeltaTime;
+        t_targetVelocity.y = rig.velocity.y;
+        rig.velocity = t_targetVelocity;
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject[] loadout;
+    public Transform weaponParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            Equip(0);
+        }
+    }
+    void Equip(int p_ind)
+    {
+        GameObject t_newEquipment = Instantiate(loadout[p_ind],weaponParent.position, weaponParent.rotation,weaponParent) as GameObject;
+        t_newEquipment.transform.localPosition = Vector3.zero;
+        t_newEquipment.transform.localEulerAngles = Vector3.zero;
+     }
+}
